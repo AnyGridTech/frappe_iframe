74f6db24c34999e9726d05c16049c535ec0d7e25
Marco <gasparmarco92@gmail.com>
2025-10-20 20:36:53 +0000

    First commit for Frappe Tooltip.

--- frappe_iframe/hooks.py
+++ frappe_iframe/hooks.py
@@ -26,7 +26,7 @@ app_license = "mit"
 
 # include js, css files in header of desk.html
 # app_include_css = "/assets/frappe_iframe/css/frappe_iframe.css"
-# app_include_js = "/assets/frappe_iframe/js/frappe_iframe.js"
+app_include_js = "/assets/frappe_iframe/js/frappe_iframe.js"
 
 # include js, css files in header of web template
 # web_include_css = "/assets/frappe_iframe/css/frappe_iframe.css"
